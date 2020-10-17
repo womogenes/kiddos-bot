d1369caf932929c568262a7e3c2857689beb7f08
William Y. Feng <williamfeng1729@gmail.com>
2020-10-17 12:17:23 -0700

    Make sure to save the points file!

--- Client/_other.py
+++ Client/_other.py
@@ -5,7 +5,7 @@ import os
 async def logout(self, message):
     await message.channel.send("""These are my last words, and I am certain that my sacrifice will not be in vain. I am certain that, at the very least, it will be a moral lesson that will punish felony, cowardice and treason. 💀⚰️""")
     os.system("git add .")
-    os.system("git commit -m 'Make sure to save the points file!'")
+    os.system('git commit -m "Make sure to save the points file!"')
     os.system("git push")
     await discord.Client.logout(self)
     
