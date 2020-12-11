1ccf52741d3a99eb28eda92ab2d33c839e975762
William Y. Feng <williamfeng1729@gmail.com>
2020-12-10 19:39:07 -0800

    Add a reaction and remove announcement purging

--- Client/_announcements.py
+++ Client/_announcements.py
@@ -1,4 +1,6 @@
 async def clear_announcements(self):
+    return
+
     def purge(m):
         return not m.mention_everyone and m.author.id != 675191775261884436
     #print("Purging...")
@@ -7,6 +9,8 @@ async def clear_announcements(self):
     
     
 async def clean_announcements(self, message):
+    return 
+    
     if message.channel != self.announcementsChannel or message.mention_everyone or 763544205774815273 in [i.id for i in message.author.roles]:
         return False
         
